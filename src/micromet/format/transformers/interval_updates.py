0b7e772ec2f45f9259e11e1b85aeadf02d3bd359
wetlandsDiane <dmenuz@utah.gov>
2025-10-15 10:55:58 -0600

    adding dict of dates when met/eddy sampling interval changed

---
+++ src/micromet/format/transformers/interval_updates.py
@@ -0,0 +1,20 @@
+"""
+This module contains the datetime when sampling freuency was 
+updated from 30 minutes to 60 minutes for eddy data (first item in list)
+and met data (second item in list).
+
+"""
+
+interval_update_dict = {
+    "US-UTE":	["10/9/2025 11:30",	"10/9/2025 11:50"],
+    "US-UTJ":	["10/9/2025 12:04",	"10/9/2025 12:14"],
+    "US-UTB":	["10/9/2025 13:08",	"10/9/2025 13:08"],
+    "US-CdM":	["10/9/2025 13:20",	"10/9/2025 13:27"],
+    "US-UTM":	["10/9/2025 13:57",	"10/10/2025 8:15"],
+    "US-UTW":	["10/10/2025 10:24", "10/10/2025 10:37"],
+    "US-UTV":	["10/14/25 6:22",	"10/14/2025 11:05"],
+    "US-UTP":	["10/13/25 12:13",	"10/13/2025 12:25"],
+    "US-UTL":	["10/10/25 1:58",	"10/10/25 1:58 AM"],
+    "US-UTZ":	["10/10/25 1:46", None],
+    "US-UTG":	["10/10/2025 7:46",	"10/10/2025 7:54"]
+}
